ba645e1dfd0c3c6cbadf184b6c5c96b46277e1bf
fredcobain <fredcobain@gmail.com>
2017-09-26 00:01:16 -0300

    alterando choose

--- es-core/src/components/ComponentGrid.cpp
+++ es-core/src/components/ComponentGrid.cpp
@@ -445,11 +445,11 @@ std::vector<HelpPrompt> ComponentGrid::getHelpPrompts()
 	}
 
 	if(canScrollHoriz && canScrollVert)
-		prompts.push_back(HelpPrompt("up/down/left/right", "choose"));
+		prompts.push_back(HelpPrompt("up/down/left/right", "navegar"));
 	else if(canScrollHoriz)
-		prompts.push_back(HelpPrompt("left/right", "choose"));
+		prompts.push_back(HelpPrompt("left/right", "navegar"));
 	else if(canScrollVert)
-		prompts.push_back(HelpPrompt("up/down", "choose"));
+		prompts.push_back(HelpPrompt("up/down", "navegar"));
 
 	return prompts;
 }
